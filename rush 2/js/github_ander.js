e7b279fc5164fcc032c108a8f3f509f8ebe10f7b
Ander Amorin sanchez <aamorin-@c3r6s5.42urduliz.com>
2021-11-22 12:37:58 +0100

    rush

---
+++ rush 2/js/github_ander.js
@@ -0,0 +1,16 @@
+$.ajax({
+	url: "https://api.github.com/users/anderamo/repos",
+	method: "GET"
+}).then(results => {
+	console.log(results)
+	let index = "<li class='active' data-target='#cc-Indicators' data-slide-to='0'></li>"
+			$("#add_li").append(index);
+	let article = "<div class='carousel-item active'><div class='row'><div style='width:100%;display:flex;justify-content:space-around;align-items:center;'><div><p style='font-size:150%'>"+ results[0].name +"</p></div><div><button ><a href=" + results[0].html_url + " target=_blank>Github repo</a></button></div></div></div>"
+			$("#add_carousel").append(article);
+	for (let i = 1; i < results.length; i++){
+			let index = "<li data-target='#cc-Indicators' data-slide-to="+i+"></li>"
+			$("#add_li").append(index);
+			let article = "<div class='carousel-item'><div class='row'><div style='width:100%;display:flex;justify-content:space-around;align-items:center;'><div><p style='font-size:150%'>"+ results[i].name +"</p></div><div><button><a href=" + results[i].html_url + " target=_blank>Github repo</a></button></div></div></div>"
+			$("#add_carousel").append(article);
+	}
+})
